855e6eada79a9d49906577b7a60581abd66a8edd
Nausher Rao <SherRao15@gmail.com>
2022-06-07 00:13:31 -0400

    Make all routes actually return proper data this time

--- src/index.js
+++ src/index.js
@@ -1,21 +1,16 @@
+require("dotenv").config();
+
 const express = require("express");
 const app = express();
 const port = 3000;
 
+const root = require("./routes/root");
 const spotify = require("./routes/spotify");
 const wakatime = require("./routes/wakatime");
 
-app.get("/", (request, response) => {
-    response.send("Hello World!");
-});
-
-app.get("/spotify", (request, response) => {
-    spotify(request, response);
-});
-
-app.get("/wakatime", (request, response) => {
-    wakatime(request, response);
-});
+app.use("/", root);
+app.use("/spotify", spotify);
+app.use("/wakatime", wakatime);
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`);
--- src/routes/wakatime.js
+++ src/routes/wakatime.js
@@ -1,4 +1,46 @@
-module.exports = (request, response) => {
-    response.send("wakatime poggers!");
+const WakaTimeClientPackage = require("wakatime-client");
+const { WakaTimeClient } = WakaTimeClientPackage;
+const { RANGE } = WakaTimeClientPackage;
 
-};
+const process = require("node:process");
+const express = require("express");
+const router = express.Router();
+
+const client = new WakaTimeClient(process.env.WAKATIME_API_KEY);
+
+router.use((request, response, next) => {
+    console.log("Time: ", Date.now());
+    next();
+});
+
+router.get("/", async (request, response) => {
+    const userDetails = await client.getMyStats({ range: RANGE.LAST_7_DAYS });
+    console.log(userDetails);
+    response.send(userDetails);
+});
+
+router.get("/commits", async (request, response) => {
+    const userDetails = await client.getMyCommits({ projectName: "LaurierHawkHacks/Landing" });
+    console.log(userDetails);
+    response.send(userDetails);
+});
+
+router.get("/languages", async (request, response) => {
+    const userDetails = await client.getMyStats({ range: RANGE.LAST_7_DAYS });
+    console.log(userDetails);
+    response.send(userDetails);
+});
+
+router.get("/projects", async (request, response) => {
+    const userDetails = await client.getMyProjects();
+    console.log(userDetails);
+    response.send(userDetails);
+});
+
+router.get("/history", async (request, response) => {
+    const userDetails = await client.getMyStats({ range: RANGE.LAST_7_DAYS });
+    console.log(userDetails);
+    response.send(userDetails);
+});
+
+module.exports = router;
